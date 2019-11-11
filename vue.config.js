9dca3e119b736fd40553e8fdf1d1bc15f0c62c81
BK1917wanghuanhuan <1835369707@qq.com>
2019-11-11 17:46:52 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const path=require("path")
+
+module.exports = {
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'http://cmsjapi.ffquan.cn',
+          changeOrigin: true
+        }
+      }
+    },
+    configureWebpack:{
+        resolve:{
+            alias:{
+                "@":path.join(__dirname,"./src"),
+                "@api":path.join(__dirname,"./src/api"),
+                "@pages":path.join(__dirname,"./src/pages"),
+                "@store":path.join(__dirname,"./src/store"),
+                "@router":path.join(__dirname,"./src/router"),
+                "@components":path.join(__dirname,"./src/components"),
+                "@utils":path.join(__dirname,"./src/utils"),
+                "@assets":path.join(__dirname,"./src/assets"),
+                "@common":path.join(__dirname,"./src/common"),
+            }
+        }
+    }
+  }
